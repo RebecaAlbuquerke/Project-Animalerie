daca18e07e7d90c2d20a3905631d6ac67f074443
Rebeca Albuquerque <rebecaalbuquerke@gmail.com>
2021-11-28 16:57:12 +0100

    charger etat

--- animalerie/models.py
+++ animalerie/models.py
@@ -2,21 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Equipement(models.Model):
-    nom_equipement= models.CharField(max_length=100, primary_key=True)
+    id_equip = models.CharField(max_length=100, primary_key=True)
     disponibilite = models.CharField(max_length=20)
     photo = models.CharField(max_length=200)
-
     def __str__(self):
-        return self.nom_equipement
-
-
+        return self.id_equip
+ 
+ 
 class Animal(models.Model):
-    nom_animal = models.CharField(max_length=100, primary_key=True)
+    id_animal = models.CharField(max_length=100, primary_key=True)
     etat = models.CharField(max_length=20)
     type = models.CharField(max_length=20)
     race = models.CharField(max_length=20)
     photo = models.CharField(max_length=200)
     lieu = models.ForeignKey(Equipement, on_delete=models.CASCADE)
-
     def __str__(self):
-        return self.nom_animal
+        return self.id_animal
--- animalerie/urls.py
+++ animalerie/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.animals, name='animals'),
-    path('', views.equipements, name='equipaments'),
+    path('animals/', views.animals, name='animals'),
+    path('equipements/', views.equipements, name='equipaments'),
+    path('animal/<str:id_animal>/', views.animal_detail, name='animal_detail'),
+    path('animal/<str:id_animal>/?<str:message>', views.animal_detail, name='animal_detail_mes'),
 ]
